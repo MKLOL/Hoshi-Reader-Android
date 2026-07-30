a19b48aaed1ae16e1e53120c84c5e7cc2dcb17a4
Dragos Ristache <ristachedragos@gmail.com>
2026-07-30 11:57:06 -0400

    chore: release 0.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,8 @@ android {
         applicationId = "moe.antimony.hoshi"
         minSdk = 28
         targetSdk = 36
-        versionCode = 909
-        versionName = "0.9.9"
+        versionCode = 1000
+        versionName = "0.10.0"
         releaseVersionCode?.let { versionCode = it }
         releaseVersionName?.let { versionName = it }
 
